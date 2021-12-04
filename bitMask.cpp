1235f70f0550c1a7d5e3925072059269b19cc1c0
SoHyeon Kim <happythgus89@gmail.com>
2021-12-04 16:09:45 +0900

    Add files via upload

---
+++ bitMask.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+/*
+
+[ 비트 마스크 ]
+
+- 연산자 우선순위기 낮다 -> 괄호를 적극적으로 사용하자 
+- 부호로 인하여 버그 발생 가능 -> 되도록이면 부호 없는 수에 대해 사용
+- 32 bit / 64 bit 오버플로우 주의 
+
+*/
+
+
+
+// 비트 마스크를 활용한 집합의 구현 
+
+int a;
+int b;
+s
