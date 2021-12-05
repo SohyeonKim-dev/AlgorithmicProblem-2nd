5120bc8646280186f74f67e82c25cc99042ca904
SoHyeon Kim <happythgus89@gmail.com>
2021-12-05 15:52:48 +0900

    Queue-stack-deque

--- 04_Queue-stack-deque/brackets.cpp
+++ 04_Queue-stack-deque/brackets.cpp
@@ -1,7 +1,59 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <list>
+#include <stack>
 using namespace std;
 
 
+/*
+
+
+		[ ¹®Á¦ ]
+
+
+Best White is a mathematics graduate student at T1 University.
+Recently, he finished writing a paper and he decided to polish it.
+As he started to read it from the beginning, he realized that some of the formulas have problems:
+some of the brackets are mismatched! Since there are so many formulas in his paper,
+he decided to check their validity with a computer program.
+
+The following kinds of brackets are included in Best White¡¯s paper.
+
+Round brackets are opened by a ( and closed by a ).
+Curly brackets are opened by a { and closed by a }.
+Square brackets are opened by a [ and closed by a ].
+A formula is said well-matched when the following conditions are met:
+
+Every bracket has a corresponding pair. ( corresponds to ), [ corresponds to ], et cetera.
+Every bracket pair is opened first, and closed later.
+No two pairs "*cross*" each other. For example, [(]) is not well-matched.
+Write a program to help Best White by checking if each of his formulas is well-matched.
+To make the problem easier, everything other than brackets are removed from the formulas.
+
+
+*/
+
+
+
+
+bool wellMatched(const string& formula) {
+	const string opening("({[");
+	const string closing(")}]");
+
+	stack <char> openStack;
+	for (int i = 0; i < formula.size(); ++i) {
+		if (opening.find(formula[i]) != -1) {
+			openStack.push(formula[i]);
+		}
+		else {
+			if (openStack.empty()){
+				return false;
+			}
+			if (opening.find(openStack.top()) != closing.find(formula[i])) {
+				return false;
+			}
+			openStack.pop();
+		}
+	}
+	return openStack.empty();
+}
