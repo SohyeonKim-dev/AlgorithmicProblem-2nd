3ff3c114ba0c28aa8ae111ad8df72d0bb1a1cf48
SoHyeon Kim <happythgus89@gmail.com>
2021-12-06 14:39:32 +0900

    String

---
+++ 05_String/KMP_algorithm.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
+vector <int> getPartialMatch(const string& N) {
+	int m = N.size();
+	vector<int> pi(m, 0);
+
+	int begin = 1; 
+	int matched = 0;
+
+	while (begin + matched < m) {
+		if (N[begin + matched] == N[matched]) {
+			++matched;
+			pi[begin + matched - 1] = matched;
+		}
+		else {
+			if (matched == 0) {
+				++begin;
+			}
+			else {
+				begin += matched - pi[matched - 1];
+				matched = pi[matched - 1];
+			}
+		}
+	}
+	return pi;
+}
+
+
+
+vector <int> kmpSearch(const string& H, const string& N) {
+	int n = H.size();
+	int m = N.size();
+	vector <int> ret;
+
+	vector <int> pi = getPartialMatch(N);
+	int begin = 0;
+	int matched = 0;
+
+	while (begin <= n - m) {
+		if (matched < m && H[begin + matched] == N[matched]) {
+			++matched;
+			if (matched == m) {
+				ret.push_back(begin);
+			}
+		}
+		else {
+			if (matched == 0) {
+				++begin;
+			}
+			else {
+				begin += matched - pi[matched - 1];
+				matched = pi[matched - 1];
+			}
+		}
+	}
+	return ret;
+}
+
+
+
+
