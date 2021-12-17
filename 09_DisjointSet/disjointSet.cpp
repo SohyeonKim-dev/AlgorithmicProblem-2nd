d38f46306f76fe9fe5f242ce7550457225e4c9c6
SoHyeon Kim <happythgus89@gmail.com>
2021-12-17 17:49:51 +0900

    DisjointSet_NAIVE

--- 09_DisjointSet/disjointSet.cpp
+++ 09_DisjointSet/disjointSet.cpp
@@ -3,3 +3,28 @@
 #include <algorithm>
 using namespace std;
 
+struct NaiveDisjointSet {
+	vector <int> parent;
+
+	NaiveDisjointSet(int n) : parent(n) {
+		for (int i = 0; i < n; ++i) {
+			parent[i] = i;
+		}
+	}
+
+	int find(int u) const {
+		if (u == parent[u]) {
+			return u;
+		}
+		return find(parent[u]);
+	}
+
+	void merge(int u, int v) {
+		u = find(u);
+		v = find(v);
+		if (u == v) {
+			return;
+		}
+		parent[u] = v;
+	}
+};
