36b9c951a412a24fd7a45d17ae51b9797decf2da
SoHyeon Kim <happythgus89@gmail.com>
2021-12-20 21:47:24 +0900

    Trie

--- 10_Trie/Trie.cpp
+++ 10_Trie/Trie.cpp
@@ -2,3 +2,21 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+
+const int ALPHABETS = 26;
+int toNumber(char ch) {
+	return ch - 'A';
+}
+
+struct TrieNode {
+	TrieNode* children[ALPHABETS];
+	bool terminal;
+
+	TrieNode() : terminal(false) {
+		memset(children, 0, sizeof(children));
+	}
+	
+	~TrieNode() {
+
+	}
+};
